b7add96f6eb4c3f1f053239a3bd874552df5a700
klimga2 <poker8702@gmail.com>
2025-08-30 11:03:27 -0500

    .

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,21 +1,24 @@
 import Brujula from "../assets/Brujula.png";
 import { useNavigate } from "react-router-dom";
-import Button from "../components/Buttons";
+import "./Start.css";
+
 const Start = () => {
   const Nav = useNavigate();
   const Go = () => {
     Nav("/Register");
   };
   return (
-    <>
-      <img src={Brujula} alt="" />
-      <h1>CREA UNA SALA</h1>
-      <p>
+    <div className="start-container">
+      <img src={Brujula} alt="Brújula" className="compass-image" />
+      <h1 className="title">CREA UNA SALA</h1>
+      <p className="description">
         ¡Forma tu equipo, enfrenta los retos y descubre si lograrás escapar!
       </p>
-
-      <button onClick={Go}>NEW GAME</button>
-    </>
+      <button onClick={Go} className="new-game-button">
+        NEW GAME
+      </button>
+    </div>
   );
 };
+
 export default Start;
